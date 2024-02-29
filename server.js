f3da443d82345fda8a2152b96734c792be15fd79
viewBrzh <viewsatview@gmail.com>
2024-02-29 13:55:39 +0700

    changed: api path

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const cors = require('cors');
 
 const pool = mysql.createPool({
     connectionLimit: 100,
@@ -13,16 +14,34 @@ const pool = mysql.createPool({
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/course/detail', (req, res) => {
-    pool.query('SELECT * FROM trn_course_detail', (error, results) => {
+app.get('/course', (req, res) => {
+  pool.query('SELECT * FROM trn_course_detail', (error, results) => {
+  if (error) {
+      res.status(500).send('Internal Server Error');
+      return;
+  }
+  res.json(results);
+  });
+});
+
+app.get('/course/detail/:id', (req, res) => {
+  const courseId = req.params.id;
+  pool.query('SELECT * FROM trn_course_detail WHERE train_course_id = ?', [courseId], (error, results) => {
     if (error) {
-        res.status(500).send('Internal Server Error');
-        return;
+      console.error('Error fetching course details:', error);
+      res.status(500).send('Internal Server Error');
+      return;
     }
-    res.json(results);
-    });
-}); 
+    if (results.length === 0) {
+      res.status(404).send('Course not found');
+      return;
+    }
+    res.json(results[0]);
+  });
+});
+
 
 const PORT = process.env.PORT || 11230;
 app.listen(PORT, () => {
