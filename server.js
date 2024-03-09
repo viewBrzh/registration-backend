2c146ea6a1c5f1228638566c30ab09b26382e695
viewBrzh <viewsatview@gmail.com>
2024-03-09 21:38:55 +0700

    added: insert update & delete api

--- server.js
+++ server.js
@@ -42,6 +42,55 @@ app.get('/course/detail/:id', (req, res) => {
   });
 });
 
+// POST route for creating a new course
+app.post('/course', (req, res) => {
+  const { courseName, courseDescription } = req.body;
+  pool.query('INSERT INTO trn_course_detail (train_course_name, train_detail) VALUES (?, ?)', [courseName, courseDescription], (error, results) => {
+    if (error) {
+      console.error('Error inserting course:', error);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    res.status(201).send('Course created successfully');
+  });
+});
+
+app.put('/course/update/:id', (req, res) => {
+  const courseId = req.params.id;
+  const { course_detail_name, train_detail, train_place, start_date, finish_date } = req.body;
+  pool.query(
+    'UPDATE trn_course_detail SET course_detail_name = ?, train_detail = ?, train_place = ?, start_date = ?, finish_date = ? WHERE train_course_id = ?',
+    [course_detail_name, train_detail, train_place, start_date, finish_date, courseId],
+    (error, results) => {
+      if (error) {
+        console.error('Error updating course:', error);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+      if (results.affectedRows === 0) {
+        res.status(404).send('Course not found');
+        return;
+      }
+      res.status(200).send('Course updated successfully');
+    }
+  );
+});
+
+
+
+// DELETE route for deleting a course by ID
+app.delete('/course/:id', (req, res) => {
+  const courseId = req.params.id;
+  pool.query('DELETE FROM trn_course_detail WHERE train_course_id = ?', [courseId], (error, results) => {
+    if (error) {
+      console.error('Error deleting course:', error);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    res.status(200).send('Course deleted successfully');
+  });
+});
+
 
 const PORT = process.env.PORT || 11230;
 app.listen(PORT, () => {
